bc7b3c943649e30f8987091390719d057854ae41
v_yyuanyyao <v_yyuanyyao@tencent.com>
2022-01-20 17:00:09 +0800

    last

---
+++ src/Lemanwang/PhpTools/TestTools.php
@@ -0,0 +1,13 @@
+<?php
+namespace Lemanwang\PhpTools;
+
+class TestTools
+{
+    public function hello(){
+        echo "hello world TestTools";
+    }
+
+    public function test(){
+        echo "测试函数";
+    }
+}

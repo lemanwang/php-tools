250082e7bec031a73a2cabbe851e61f0adbe9b7f
v_yyuanyyao <v_yyuanyyao@tencent.com>
2022-01-21 18:31:12 +0800

    last

---
+++ src/Lemanwang/PhpTools/Request/Visitor.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by : PhpStorm
+ * User: UncleFreak <00@z88j.com>
+ * Date: 2022/1/21
+ * Time: 17:39
+ */
+
+namespace Lemanwang\PhpTools\Request;
+
+
+class Visitor
+{
+    /**
+     * 获取浏览器名称
+     * Author: UncleFreak <00@z88j.com>
+     * @return string
+     */
+    public static function getBrowserName()
+    {
+        $br = '';
+        if (!empty($_SERVER['HTTP_USER_AGENT'])) {
+            $br = $_SERVER['HTTP_USER_AGENT'];
+            if (preg_match('/MSIE/i', $br)) {
+                $br = 'MSIE';
+            } elseif (preg_match('/Firefox/i', $br)) {
+                $br = 'Firefox';
+            } elseif (preg_match('/Chrome/i', $br)) {
+                $br = 'Chrome';
+            } elseif (preg_match('/Safari/i', $br)) {
+                $br = 'Safari';
+            } elseif (preg_match('/Opera/i', $br)) {
+                $br = 'Opera';
+            } else {
+                $br = 'Other';
+            }
+        }
+        return $br;
+    }
+}
